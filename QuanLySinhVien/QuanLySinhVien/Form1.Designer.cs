587950ad90ec13ceb32321ee48bd41bc91177194
Baorrrr <ddpao26@gmail.com>
2026-05-14 03:21:50 +0700

    chức năng đăng nhập

---
+++ QuanLySinhVien/QuanLySinhVien/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace QuanLySinhVien
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_username = new System.Windows.Forms.TextBox();
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.btn_dangnhap = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(152, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(505, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "QUẢN LÝ SINH VIÊN";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.Location = new System.Drawing.Point(170, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tên Đăng Nhập";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label3.Location = new System.Drawing.Point(172, 212);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Mật Khẩu";
+            // 
+            // txt_username
+            // 
+            this.txt_username.Location = new System.Drawing.Point(397, 159);
+            this.txt_username.Name = "txt_username";
+            this.txt_username.Size = new System.Drawing.Size(213, 20);
+            this.txt_username.TabIndex = 3;
+            // 
+            // txt_password
+            // 
+            this.txt_password.Location = new System.Drawing.Point(397, 212);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.Size = new System.Drawing.Size(213, 20);
+            this.txt_password.TabIndex = 4;
+            // 
+            // btn_dangnhap
+            // 
+            this.btn_dangnhap.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn_dangnhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btn_dangnhap.ForeColor = System.Drawing.Color.Red;
+            this.btn_dangnhap.Location = new System.Drawing.Point(319, 301);
+            this.btn_dangnhap.Name = "btn_dangnhap";
+            this.btn_dangnhap.Size = new System.Drawing.Size(165, 55);
+            this.btn_dangnhap.TabIndex = 5;
+            this.btn_dangnhap.Text = "Đăng Nhập";
+            this.btn_dangnhap.UseVisualStyleBackColor = false;
+            this.btn_dangnhap.Click += new System.EventHandler(this.btn_dangnhap_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_dangnhap);
+            this.Controls.Add(this.txt_password);
+            this.Controls.Add(this.txt_username);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_username;
+        private System.Windows.Forms.TextBox txt_password;
+        private System.Windows.Forms.Button btn_dangnhap;
+    }
+}
+
---
+++ QuanLySinhVien/QuanLySinhVien/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLySinhVien
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_dangnhap_Click(object sender, EventArgs e)
+        {
+            // 1. Lấy dữ liệu từ TextBox
+            string username = txt_username.Text.Trim();
+            string password = txt_password.Text.Trim();
+
+            // 2. Kiểm tra dữ liệu trống
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ tên đăng nhập và mật khẩu!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // 3. Kiểm tra thông tin đăng nhập (Ví dụ fix cứng hoặc check DB)
+            if (username == "0180866@st.huce.edu.vn" && password == "0180866")
+            {
+                MessageBox.Show("Đăng nhập thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                // Tại đây bạn có thể mở Form chính của ứng dụng
+                // MainMenu formMain = new MainMenu();
+                // formMain.Show();
+                // this.Hide(); 
+            }
+            else
+            {
+                MessageBox.Show("Tên đăng nhập hoặc mật khẩu không chính xác!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txt_password.Clear();
+                txt_username.Focus();
+            }
+        }
+    }
+}
